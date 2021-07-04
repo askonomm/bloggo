0891f0bd5afa3f30c14b81b7c7aa79add94214d9
Asko Nõmm <asko@bien.ee>
2021-07-04 15:20:20 -0300

    Add comment

--- src/bloggo/hbs_helpers.cljs
+++ src/bloggo/hbs_helpers.cljs
@@ -1,7 +1,11 @@
 (ns bloggo.hbs-helpers
   (:require [bloggo.data.config :refer [get-config]]))
 
-(defn config [key]
+(defn config 
+  "Returns a configuration value by a given `key` 
+  directly from the database. If not found, returns 
+  an empty string instead."
+  [key]
   (if-let [config-item (get-config key)]
     (get config-item :value)
     ""))
