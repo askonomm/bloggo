f6d741856f4c56f18e8408906a6222046f78b76b
Asko Nõmm <asko@bien.ee>
2021-07-04 19:56:00 -0300

    Correct docblock.

--- src/bloggo/routes/admin.cljs
+++ src/bloggo/routes/admin.cljs
@@ -3,9 +3,10 @@
   (:require-macros [bloggo.macros :refer [redirect session]]))
 
 (defn get!
-  "Checks if the application has any users, because
-  if there aren't any, then that means we are dealing
-  with a fresh install and need to therefore go to set up.
+  "Checks if the application is set up by checking if the 
+  database returns the version of the application, because if
+  it doesn't, then that means we are dealing with a fresh install 
+  and need to therefore go and set it up.
   
   If that's not the case, then we check if the user has a session.
   If the user does not have a session, we redirect to Sign In.
