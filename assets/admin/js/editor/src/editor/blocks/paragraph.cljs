539b9ef2659f6b55eff8fb395552766f9f9a8637
Asko Nõmm <asko@bien.ee>
2021-07-13 00:48:44 -0300

    Fix a few caret placement related bugs, and add comments

--- assets/admin/js/editor/src/editor/blocks/paragraph.cljs
+++ assets/admin/js/editor/src/editor/blocks/paragraph.cljs
@@ -3,19 +3,59 @@
             [re-frame.core :refer [dispatch]]
             [editor.utils :as utils]))
 
-(defn on-key-press! [event]
+(defn on-key-press!
+  "Detect if the user pressed the `enter` key or not. If
+  the user did, we want to disable the default behaviour, which
+  usually is creating a new line, and instead create a new 
+  paragraph block below it."
+  [event]
   (if (or (= "Enter" (.-key event))
           (= 13 (.-keyCode event)))
     (.preventDefault event)
     (prn "enter was not pressed")))
 
-(defn on-input! [index event]
-  (dispatch
-   [:update-paragraph-block
-    {:position index
-     :content (utils/parse-html (.-innerHTML (.-target event)))}]))
+(defn on-input!
+  "Whenever a key is pressed, we want to update the `content-state` atom 
+  so that whenever a `on-paste` event occurs, it would take correct 
+  content as the basis of which to calculate where to paste the clip.
+  
+  Reason being that whenever you type new text into the `contentEditable`, 
+  even though we do update the global state, it doesn't really update 
+  that component's inner `content-state` atom, and so when a paste 
+  happens it only takes into account the initial state of it, and not the 
+  new one, resulting in cursor placement then going wrong and setting in 
+  the beginning of the text instead of the end of the paste location, 
+  because the pasted index is further away than the actual content has 
+  indexes.
+  
+  Oh, almost forgot, we also update the `caret-location-state`, so that 
+  the component would trigger caret positioning (which it does only if 
+  the `caret-location-state` is not `nil` and the `content-state` is as 
+  long or longer than `caret-location-state`). This is needed because 
+  after updating `content-state`, the component re-renders, and the caret
+  location is lost and we want to set it in the right place again."
+  [content-state caret-location-state index event]
+  (let [content (utils/parse-html (.-innerHTML (.-target event)))]
+    (reset! content-state content)
+    (reset! caret-location-state (count content))
+    (dispatch
+     [:update-paragraph-block
+      {:position index
+       :content content}])))
 
-(defn on-paste! [content-state caret-location-state event]
+(defn on-paste!
+  "Prevents the default behaviour of a paste event from happening, and 
+  instead reads the contents of the clipboard, parses it (to remove any
+  horrible mark-up that might come with it), and then inserts it at the
+  desired position inside and updates the `content-state` atom with the
+  new content, as well as updates the `caret-location-state` atom with 
+  the new caret position which is what the caret location was when 
+  pasting + the length of the pasted content.
+  
+  We update the `caret-location-state` atom so that we could do caret 
+  placement on a `:component-did-update` event, because otherwise the 
+  caret placement would break on paste."
+  [content-state caret-location-state event]
   (.preventDefault event)
   (.then
    (.readText (.-clipboard js/navigator))
@@ -28,26 +68,50 @@
        (reset! content-state new-content)
        (reset! caret-location-state new-caret-location)))))
 
+(defn place-caret!
+  "Places a caret at the desired `caret-location-state` position
+  inside the `ref` element. But only does it if the length of 
+  `content-state` is the same or exceeds `caret-location-state`. This 
+  is because we cannot place a caret in an index that does not exist i.e
+  is out of range.
+  
+  Once the caret has been placed in its position, we reset the 
+  `caret-location-state` to `nil`, so that this placement would not occur 
+  on every render, but only when we set the `caret-location-state` to 
+  something other than `nil` (and, like said before, `content-state` 
+  either is the same or exceeds `caret-location-state`).
+  "
+  [ref content-state caret-location-state]
+  (when (and (not (nil? @caret-location-state))
+             (>= (count @content-state) @caret-location-state))
+    (let [selection (.getSelection js/window)
+          range (.createRange js/document)]
+      (.setStart range (first (.-childNodes @ref)) @caret-location-state)
+      (.collapse range true)
+      (.removeAllRanges selection)
+      (.addRange selection range)
+      (.focus @ref)
+      (reset! caret-location-state nil))))
+
+(defn render
+  "Renders the actual DOM output of the paragraph block and hooks to it
+  many of its necessary events."
+  [ref content-state caret-location-state index]
+  [:div.paragraph-content
+   {:contentEditable true
+    :data-placeholder "Start writing a paragraph ..."
+    :ref (fn [el] (reset! ref el))
+    :on-key-press #(on-key-press! %)
+    :on-input #(on-input! content-state caret-location-state index %)
+    :on-paste #(on-paste! content-state caret-location-state %)
+    :dangerouslySetInnerHTML {:__html @content-state}}])
+
 (defn block [index block]
   (let [ref (r/atom nil)
         content-state (r/atom (get block :content))
         caret-location-state (r/atom nil)]
     (r/create-class
      {:component-did-update
-      (fn []
-        (let [selection (.getSelection js/window)
-              range (.createRange js/document)]
-          (.setStart range (first (.-childNodes @ref)) @caret-location-state)
-          (.collapse range true)
-          (.removeAllRanges selection)
-          (.addRange selection range)
-          (.focus @ref)))
+      (place-caret! ref content-state caret-location-state)
       :reagent-render
-      (fn []
-        [:div.paragraph-content
-         {:contentEditable true
-          :ref (fn [el] (reset! ref el))
-          :on-key-press #(on-key-press! %)
-          :on-input #(on-input! index %)
-          :on-paste #(on-paste! content-state caret-location-state %)
-          :dangerouslySetInnerHTML {:__html @content-state}}])})))
+      (render ref content-state caret-location-state index)})))
