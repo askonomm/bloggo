cb723c6c6f337c23b56ceddbc7449a44a05ac378
Asko Nõmm <asko@bien.ee>
2021-07-13 01:36:09 -0300

    Rename heading-big to heading

---
+++ assets/admin/js/editor/src/editor/blocks/heading.cljs
@@ -0,0 +1,30 @@
+(ns editor.blocks.heading
+  (:require
+   [reagent.core :as r]
+   [re-frame.core :refer [dispatch]]))
+
+(defn on-key-press [event]
+  (if (or (= "Enter" (.-key event))
+          (= 13 (.-keyCode event)))
+    (.preventDefault event)
+    (prn "enter was not pressed")))
+
+(defn on-change [index height event]
+  (dispatch
+   [:update-heading-block
+    {:position index
+     :content (.-value (.-target event))}])
+  (reset! height (.-scrollHeight (.-target event))))
+
+(defn block [index block]
+  (let [height (r/atom 30)]
+    (fn []
+      [:textarea
+       {:default-value (get block :content)
+        :placeholder "Start writing a heading ..."
+        :style {:height (str @height "px")}
+        :ref (fn [el]
+               (when el
+                 (reset! height (.-scrollHeight el))))
+        :on-key-press #(on-key-press %)
+        :on-change #(on-change index height %)}])))
